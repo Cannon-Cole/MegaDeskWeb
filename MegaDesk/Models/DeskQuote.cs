c07f22f7849048db8885c1f2e88a040547b89dcd
Kevin Thompson <vangpao@hotmail.com>
2018-11-13 20:19:00 -0800

    Added untested validation, fixed errors for models, but unable to run. Are we missing CSS?

--- MegaDesk/Models/DeskQuote.cs
+++ MegaDesk/Models/DeskQuote.cs
@@ -5,15 +5,33 @@ namespace MegaDesk.Models
     public class DeskQuote
     {
         public int ID { get; set; }
+
+        [Range(24, 96)]
+        [Required]
         public int Width { get; set; }
+
+        [Range(12, 48)]
+        [Required]
         public int Depth { get; set; }
+
+        [Range(0, 7)]
+        [Required]
         public int Drawers { get; set; }
+
+        [Required]
         public string Material { get; set; }
+
+        [Required]
         public int Rush { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [Required]
         public DateTime Date { get; set; }
+
         public int Total { get; set; }
     }
 }
