88d000e9e182534b3b2acc2e0c1a25b1e94cc95e
Kevin Thompson <vangpao@hotmail.com>
2018-11-13 21:46:08 -0800

    Added ViewStart and ViewImport, the pages are now viewed correctly

---
+++ MegaDesk/Migrations/20181114051214_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MegaDesk.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeskQuote",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Width = table.Column<int>(nullable: false),
+                    Depth = table.Column<int>(nullable: false),
+                    Drawers = table.Column<int>(nullable: false),
+                    Material = table.Column<string>(nullable: false),
+                    Rush = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Total = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeskQuote", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeskQuote");
+        }
+    }
+}
